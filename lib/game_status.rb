3c64bea6d93e231fd866a9009b073dce0311b360
Vicki Hamm <vicki.cello@gmail.com>
2018-06-13 17:41:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,9 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      puts win_combination
+      win_combination
+    elsif elsif position_1 =="O" && position_2 =="O" && position_3 =="O"
+      win_combination
     else
       false
     end
